0e2f68f5f4ef836b6213b987576994cbdb459fac
nlarranagac <nlarranagac@unal.edu.co>
2017-10-02 23:47:12 -0500

    Reto 6 Changing the Orientation and Saving State

---
+++ Reto6/app/src/main/java/co/edu/unal/nlarranagac/reto3/logic/TicTacToeGame.java
@@ -0,0 +1,182 @@
+package co.edu.unal.nlarranagac.reto3.logic;
+
+import java.util.Random;
+
+/**
+ * Created by angee on 24/08/2017.
+ */
+
+public class TicTacToeGame {
+    private char mBoard[];
+    public final static int BOARD_SIZE = 9;
+
+    public static final char HUMAN_PLAYER = 'X';
+    public static final char COMPUTER_PLAYER = 'O';
+    public static final char EMPTY_SPACE = ' ';
+    public static final int NO_WINNER_OR_TIE_YET = 0;
+    public static final int TIE = 1;
+    public static final int PLAYER_WON = 2;
+    public static final int COMPUTER_WON = 3;
+
+    private Random mRand;
+
+    private DifficultyLevel mDifficultyLevel = DifficultyLevel.Expert;
+    private int randomMove;
+
+    public DifficultyLevel getDifficultyLevel() {
+        return mDifficultyLevel;
+    }
+
+    public void setDifficultyLevel(DifficultyLevel mDifficultyLevel) {
+        this.mDifficultyLevel = mDifficultyLevel;
+    }
+
+    public TicTacToeGame() {
+        mBoard = new char[BOARD_SIZE];
+        for (int i = 0; i < BOARD_SIZE; i++)
+            mBoard[i] = EMPTY_SPACE;
+        mRand = new Random();
+    }
+
+
+    public void clearBoard() {
+        for (int i = 0; i < BOARD_SIZE; i++){
+            mBoard[i] = EMPTY_SPACE;
+        }
+    }
+
+    public void setMove(char player, int location){
+        mBoard[location] = player;
+    }
+
+
+    public int getComputerMove() {
+        int move = -1;
+        if (mDifficultyLevel == DifficultyLevel.Easy) {
+            move = getRandomMove();
+        } else if (mDifficultyLevel == DifficultyLevel.Harder) {
+            move = getWinningMove();
+
+        } else if (mDifficultyLevel == DifficultyLevel.Expert) {
+            move = getWinningMove();
+            if (move == -1)
+                move = getBlockingMove();
+            if (move == -1)
+                move = getRandomMove();
+        }
+        return move;
+    }
+    public int getWinningMove(){
+        int move = -1;
+
+        // First see if there's a move O can make to win
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER) {
+                char curr = mBoard[i];
+                mBoard[i] = COMPUTER_PLAYER;
+                if (checkForWinner() == COMPUTER_WON) {
+                    setMove(COMPUTER_PLAYER, i);
+                    return i;
+                }
+                else
+                    mBoard[i] = curr;
+            }
+        }
+
+        return move;
+
+    }
+
+
+    // Check for a winner.  Return
+    //  0 if no winner or tie yet
+    //  1 if it's a tie
+    //  2 if X won
+    //  3 if O won
+    public int checkForWinner() {
+
+        // Check horizontal wins
+        for (int i = 0; i <= 6; i += 3)	{
+            if (mBoard[i] == HUMAN_PLAYER &&
+                    mBoard[i+1] == HUMAN_PLAYER &&
+                    mBoard[i+2]== HUMAN_PLAYER)
+                return PLAYER_WON;
+            if (mBoard[i] == COMPUTER_PLAYER &&
+                    mBoard[i+1]== COMPUTER_PLAYER &&
+                    mBoard[i+2] == COMPUTER_PLAYER)
+                return COMPUTER_WON;
+        }
+
+        // Check vertical wins
+        for (int i = 0; i <= 2; i++) {
+            if (mBoard[i] == HUMAN_PLAYER &&
+                    mBoard[i+3] == HUMAN_PLAYER &&
+                    mBoard[i+6]== HUMAN_PLAYER)
+                return PLAYER_WON;
+            if (mBoard[i] == COMPUTER_PLAYER &&
+                    mBoard[i+3] == COMPUTER_PLAYER &&
+                    mBoard[i+6]== COMPUTER_PLAYER)
+                return COMPUTER_WON;
+        }
+
+        // Check for diagonal wins
+        if ((mBoard[0] == HUMAN_PLAYER &&
+                mBoard[4] == HUMAN_PLAYER &&
+                mBoard[8] == HUMAN_PLAYER) ||
+                (mBoard[2] == HUMAN_PLAYER &&
+                        mBoard[4] == HUMAN_PLAYER &&
+                        mBoard[6] == HUMAN_PLAYER))
+            return PLAYER_WON;
+        if ((mBoard[0] == COMPUTER_PLAYER &&
+                mBoard[4] == COMPUTER_PLAYER &&
+                mBoard[8] == COMPUTER_PLAYER) ||
+                (mBoard[2] == COMPUTER_PLAYER &&
+                        mBoard[4] == COMPUTER_PLAYER &&
+                        mBoard[6] == COMPUTER_PLAYER))
+            return COMPUTER_WON;
+
+        // Check for tie
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            // If we find a number, then no one has won yet
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER)
+                return NO_WINNER_OR_TIE_YET;
+        }
+
+        // If we make it through the previous loop, all places are taken, so it's a tie
+        return TIE;
+    }
+
+    public int getRandomMove() {
+
+        int move = -1;
+        // Generate random move
+        do
+        {
+            move = mRand.nextInt(BOARD_SIZE);
+        } while (mBoard[move] == HUMAN_PLAYER || mBoard[move] == COMPUTER_PLAYER);
+
+        setMove(COMPUTER_PLAYER, move);
+        return move;
+    }
+
+    public int getBlockingMove() {
+        int move = -1;
+        // See if there's a move O can make to block X from winning
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER) {
+                char curr = mBoard[i];   // Save the current number
+                mBoard[i] = HUMAN_PLAYER;
+                if (checkForWinner() == PLAYER_WON) {
+                    setMove(COMPUTER_PLAYER, i);
+                    return i;
+                }
+                else
+                    mBoard[i] = curr;
+            }
+        }
+
+        return move;
+    }
+
+    public enum DifficultyLevel{ Easy, Harder, Expert};
+}
